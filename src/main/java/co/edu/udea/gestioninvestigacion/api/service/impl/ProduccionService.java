8dfc173071a77c9dd5b8f4aeaa9a6b30f08c06c0
LUIS F RIOS - UDEA <lfernando.rios@udea.edu.co>
2019-09-24 23:51:31 -0500

    Tipo Produccion: repositorio, servicio y controlador

--- src/main/java/co/edu/udea/gestioninvestigacion/api/service/impl/ProduccionService.java
+++ src/main/java/co/edu/udea/gestioninvestigacion/api/service/impl/ProduccionService.java
@@ -132,10 +132,11 @@ public class ProduccionService implements ProduccionServiceInt{
                 categoriasPorProduccionRepository.save(categoriasPorProduccion);
             }
         }
-        TipoProduccion tempTipoProduccion = new TipoProduccion();
+        /* ya se hace desde el front end*/
+        /*TipoProduccion tempTipoProduccion = new TipoProduccion();
         tempTipoProduccion.setTipoProdID(2);
         tempTipoProduccion.setDescripcion("Trabajo de Grado");
-        produccion.setTipoProduccion(tempTipoProduccion);
+        produccion.setTipoProduccion(tempTipoProduccion);*/
         return produccionRepository.save(produccion);
     };
 }
---
+++ src/main/java/co/edu/udea/gestioninvestigacion/api/service/impl/TProduccionServiceInt.java
@@ -0,0 +1,9 @@
+package co.edu.udea.gestioninvestigacion.api.service.impl;
+
+import co.edu.udea.gestioninvestigacion.api.model.TipoProduccion;
+
+import java.util.List;
+
+public interface TProduccionServiceInt {
+    public List<TipoProduccion> getTProducciones();
+}
